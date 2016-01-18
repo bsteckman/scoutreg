428d2ff6a9462e7166f0031c2ab6928b707b51ce
Zmixon91 <zmixon91@gmail.com>
2016-01-18 13:02:06 -0700

    Removed Contact from app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,9 @@
-function Registerant(firstName, lastName, position, email, phoneNumber, contactNumber, healthForm, dateRegistered, shirtSize, isAdult) {
+function Registrant(firstName, lastName, position, email, phoneNumber, healthForm, dateRegistered, shirtSize, isAdult) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.position = position;
     this.email = email;
     this.phoneNumber=phoneNumber;
-    this.contactNumber=contactNumber;
     this.healthForm = healthForm;
     this.dateRegistered = dateRegistered;
     this.shirtSize = shirtSize;
