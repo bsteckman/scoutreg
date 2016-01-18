1d22cf8c4017f564012ab8dffeeeb748f581f42c
LarryHillyer <LarryHillyer@hotmail.com>
2016-01-18 12:10:53 -0700

    Create Initial Objects

---
+++ assets/scripts/app.js
@@ -0,0 +1,18 @@
+function Registerant(firstName, lastName, position, email, phoneNumber, contactNumber, healthForm, dateRegistered, shirtSize, isAdult) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.position = position;
+    this.email = email;
+    this.phoneNumber=phoneNumber;
+    this.contactNumber=contactNumber;
+    this.healthForm = healthForm;
+    this.dateRegistered = dateRegistered;
+    this.shirtSize = shirtSize;
+    this.isAdult = isAdult;
+     
+}
+
+function formData(packNumber, campNumber) {
+    this.packNumber = packNumber;
+    this.campNumber = campNumber;
+}
