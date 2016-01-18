2c7cad56533dc5493f137e20bd53278a8845f2a1
LarryHillyer <LarryHillyer@hotmail.com>
2016-01-18 14:16:31 -0700

    added adult as registrant

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,17 @@
 var registrantList = []
 
-function Registrant(firstName, lastName, position, email, phoneNumber, healthForm, dateRegistered, shirtSize, isAdult) {
+function Registrant(firstName, lastName, position, email, phoneNumber, isAdult) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.position = position;
     this.email = email;
     this.phoneNumber = phoneNumber;
-    this.healthForm = healthForm;
-    this.dateRegistered = dateRegistered;
-    this.shirtSize = shirtSize;
-    this.isAdult = isAdult;
+    this.isAdult = 'true';
+    
+    // this.healthForm = healthForm;
+    // this.dateRegistered = dateRegistered;
+    // this.shirtSize = shirtSize;
+    // this.isAdult = isAdult;
 
 }
 
@@ -21,10 +23,27 @@ function Auth(packNumber, email) {
 function addAdult() {
 
     var adult = document.querySelectorAll("input.registrant");
+    var firstName;
+    var lastName;
+    var position;
+    var email;
+    var cellPhone;
+    
     for (var i = 0; i < adult.length; i++) {
-        console.log(adult[i].value);
+       
+        if (adult[i].id === 'firstContactName') {
+            firstName = adult[i].value;
+        } else if (adult[i].id === 'lastContactName') {
+            lastName = adult[i].value;
+        } else if (adult[i].id === 'position') {
+            position = adult[i].value;
+        } else if (adult[i].id === 'email') {
+            email = adult[i].value;
+        } else if (adult[i].id === 'adultCellPhone') {
+            cellPhone = adult[i].value;
+        }              
     }
-
+    var adult1 = new Registrant(firstName, lastName, position, email, cellPhone, isAdult)
 }
 
 function addScout() {
