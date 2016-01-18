cf4c314983d7e0667156d0eed6b8b88a320bce6e
Zmixon91 <zmixon91@gmail.com>
2016-01-18 16:07:25 -0700

    refactored form layout

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,50 +17,32 @@ function Auth(packNumber, email) {
     this.email = email;
 }
 
-function addRegistrant() {
 
-    var registrant = document.querySelectorAll("input.registrant");
-    var firstName;
-    var lastName;
-    var position;
-    var email;
-    var cellPhone;
+function addContact(e, form){
+    e.preventDefault();
+    
+    //pass to constructor 
+    var firstName = form['firstName'].value;
+    var lastName = form['lastName'].value;
+    var position = form['position'].value;
+    var email = form['email'].value;
+    var phoneNumber = form['phoneNumber'].value;
     var isAdult = true;
-    
-    for (var i = 0; i < registrant.length; i++) {
-       
-        if (registrant[i].id === 'firstContactName') {
-            firstName = registrant[i].value;
-        } else if (registrant[i].id === 'lastContactName') {
-            lastName = registrant[i].value;
-        } else if (registrant[i].id === 'position') {
-            position = registrant[i].value;
-        } else if (registrant[i].id === 'email') {
-            email = registrant[i].value;
-        } else if (registrant[i].id === 'cellPhone') {
-            cellPhone = registrant[i].value;
-        } else if (registrant[i].id === 'isAdult'){
-            isAdult = registrant[i].value;
-        }        
-    }
-    var registrant1 = new Registrant(firstName, lastName, position, email, cellPhone, isAdult)
-    registrantList.push(registrant1)
-    
-    update();
+    var registrant1 = new Registrant(firstName, lastName, position, email, phoneNumber, isAdult);
+    registrantList.push(registrant1);
 }
 
 function update() {
-    if (registrantList.length  <=1) {
+    if (registrantList.length <= 1) {
         addSecondAdult();
-    } else if (registrantList.length <=2) {
+    } else if (registrantList.length <= 2) {
         addTableLogic();
     }
 }
 
 function addSecondAdult() {
-    
 }
 
 function addTableLogic() {
-    
+
 }
