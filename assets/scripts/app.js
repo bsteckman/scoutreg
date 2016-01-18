837bf7f457319c64daa29680ea67497f8f7dff05
Zmixon91 <zmixon91@gmail.com>
2016-01-18 16:23:40 -0700

    small changes to ready for modal and table

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,42 +7,37 @@ function Registrant(firstName, lastName, position, email, phoneNumber, isAdult) 
     this.email = email;
     this.phoneNumber = phoneNumber;
     this.isAdult = isAdult;
+    
+    // Add these later
     // this.healthForm = healthForm;
     // this.dateRegistered = dateRegistered;
     // this.shirtSize = shirtSize;
 }
 
-function Auth(packNumber, email) {
-    this.packNumber = packNumber;
-    this.email = email;
-}
+// function Auth(packNumber, email) {
+//     this.packNumber = packNumber;
+//     this.email = email;
+// }
 
 
 function addContact(e, form){
+    // Prevent page reload
     e.preventDefault();
-    
-    //pass to constructor 
+    // define values
     var firstName = form['firstName'].value;
     var lastName = form['lastName'].value;
     var position = form['position'].value;
     var email = form['email'].value;
     var phoneNumber = form['phoneNumber'].value;
     var isAdult = true;
+    //pass to constructor 
     var registrant1 = new Registrant(firstName, lastName, position, email, phoneNumber, isAdult);
     registrantList.push(registrant1);
+    // clear form
+    form.reset();
+    form.setAttribute("style","display: none");
 }
 
 function update() {
-    if (registrantList.length <= 1) {
-        addSecondAdult();
-    } else if (registrantList.length <= 2) {
-        addTableLogic();
-    }
-}
-
-function addSecondAdult() {
-}
-
-function addTableLogic() {
-
+    // write registrantList to table
 }
