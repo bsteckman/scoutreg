9dcb7994f8accad676be48b7255e900ce017e946
Zmixon91 <zmixon91@gmail.com>
2016-01-21 14:41:15 -0700

    fixed shirt tally

--- assets/scripts/registration.js
+++ assets/scripts/registration.js
@@ -1,15 +1,7 @@
 //Shirt size prices
 var shirtPrice = 10.00;
-var shitPriceXl = 12.00;
-//Shirt sizes for Adults
-var xxlShirts = 0;
-var xlShirts = 0;
-var lgShirts = 0;
-var mdShirts = 0;
-var smShirts = 0;
-//Shirt sizes for youth
-var ylgShirts = 0;
-var ymdShirts = 0;
+var shirtPriceXXL = 12.00;
+
 
 function DayCamp(camp, program, dates, days) {
     this.camo = camp;
@@ -72,7 +64,7 @@ DayCamps.push(camp24);
 
 var registrantList = [];
 
-function Registrant(firstName, lastName, position, email, phoneNumber, isAdult) {
+function Registrant(firstName, lastName, position, email, phoneNumber, isAdult, tShirt) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.position = position;
@@ -80,19 +72,12 @@ function Registrant(firstName, lastName, position, email, phoneNumber, isAdult) 
     this.phoneNumber = phoneNumber;
     this.isAdult = isAdult;
     this.contact = null;
+    this.tShirt = tShirt;
 
     
     // Add these later
     // this.healthForm = healthForm;
-    // this.dateRegistered = dateRegistered;
-    // this.shirtSize = shirtSize;
 }
-
-// function Auth(packNumber, email) {
-//     this.packNumber = packNumber;
-//     this.email = email;
-// }
-
 
 function addRegistrant(e, form) {
     // Prevent page reload
@@ -104,8 +89,9 @@ function addRegistrant(e, form) {
     var email = form['email'].value;
     var phoneNumber = form['phoneNumber'].value;
     var isAdult = true;
+    var tShirt = form['tShirt'].value
     //pass to constructor 
-    var registrant1 = new Registrant(firstName, lastName, position, email, phoneNumber, isAdult);
+    var registrant1 = new Registrant(firstName, lastName, position, email, phoneNumber, isAdult, tShirt);
     registrantList.push(registrant1);
     // clear form
     form.reset();
@@ -123,6 +109,15 @@ function addRegistrant(e, form) {
 function update() {
     //get html elements
     var myTable = document.getElementById("my-table");
+    //Shirt sizes for Adults
+    var xxlShirts = 0;
+    var xlShirts = 0;
+    var lgShirts = 0;
+    var mdShirts = 0;
+    var smShirts = 0;
+    //Shirt sizes for youth
+    var ylgShirts = 0;
+    var ymdShirts = 0;
     //iterate over the list
     for (var i = 0; i < registrantList.length; i++) {
         //read object 
@@ -131,34 +126,49 @@ function update() {
         console.log(currentUser.firstName);
         
         //junk
-        if (currentUser.shirtSize) {
-            switch(currentUser.shirtSize) {
-                case "Adult-2xl":
-                xxlShirts++;
-                break;
+        if (currentUser.tShirt) {
+            switch (currentUser.tShirt) {
+                case "Adult-xxl":
+                    xxlShirts++;
+                    break;
                 case "Adult-xl":
-                xlShirts++;
-                break;
+                    xlShirts++;
+                    break;
                 case "Adult-lg":
-                lgShirts++;
-                break;
+                    lgShirts++;
+                    break;
                 case "Adult-md":
-                mdShirts++;
-                break;
+                    mdShirts++;
+                    break;
                 case "Adult-sm":
-                smShirts++;
-                break;
+                    smShirts++;
+                    break;
                 case "youth-lg":
-                ylgShirts++;
-                break;
+                    ylgShirts++;
+                    break;
                 case "youth-md":
-                ymdShirts++;
-                break;        
+                    ymdShirts++;
+                    break;
             }
         }
+        // document.getElementById("shirt-sizes").innerHTML = String(xxlShirts);
+        // document.getElementById("shirt-prices").innerHTML = String(xxlShirts * shirtPriceXXL)
+        
+        document.getElementById("xxl-shirt-sizes").innerHTML = String("Total XXL Shirts: " + xxlShirts);
+        document.getElementById("xxl-shirt-prices").innerHTML = String("Total XXL Shirt Price: " + xxlShirts * shirtPriceXXL);
+        document.getElementById("xl-shirt-sizes").innerHTML = String("Total XL Shirts: " + xlShirts);
+        document.getElementById("xl-shirt-prices").innerHTML = String("Total XL Shirt Price: " + xlShirts * shirtPrice);
+        document.getElementById("md-shirt-sizes").innerHTML = String("Total MD Shirts: " + mdShirts);
+        document.getElementById("md-shirt-prices").innerHTML = String("Total MD Shirt Price: " + mdShirts * shirtPrice);
+        document.getElementById("sm-shirt-sizes").innerHTML = String("Total SM Shirts: " + smShirts);
+        document.getElementById("sm-shirt-prices").innerHTML = String("Total SM Shirt Price: " + smShirts * shirtPrice);
+        document.getElementById("ylg-shirt-sizes").innerHTML = String("Total YLG Shirts: " + ylgShirts);
+        document.getElementById("ylg-shirt-prices").innerHTML = String("Total YLG Shirt Price: " + ylgShirts * shirtPrice);
+        document.getElementById("ymd-shirt-sizes").innerHTML = String("Total YMD Shirts: " + ymdShirts);
+        document.getElementById("ymd-shirt-prices").innerHTML = String("Total YMD Shirt Price: " + ymdShirts * shirtPrice);
+        document.getElementById("total-shirt-prices").innerHTML = String("Total price is " + (xxlShirts * shirtPriceXXL + xlShirts * shirtPrice + mdShirts * shirtPrice + smShirts * shirtPrice + ylgShirts * shirtPrice + ymdShirts * shirtPrice));
         // document.write("shirt tally xl") = xlShirts
         // document.write("shirt price xl") = shirtPriceXL * xlShirts
     }
-
 
 }
